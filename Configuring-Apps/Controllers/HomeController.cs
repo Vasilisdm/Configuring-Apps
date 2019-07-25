7865bf817849ec01b040cc253183d344e3f9441c
vasilisdmr <vasilisdmr@gmail.com>
2019-07-25 20:35:00 +0300

    Inserted a missing semi colon.

--- Configuring-Apps/Controllers/HomeController.cs
+++ Configuring-Apps/Controllers/HomeController.cs
@@ -5,8 +5,9 @@ namespace Configuring_Apps.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() => View(new Dictionary<string, string> {
+        public ViewResult Index() => View(new Dictionary<string, string>
+        {
             ["Message"] = "This is the Index action method."
-        })
+        });
     }
 }
