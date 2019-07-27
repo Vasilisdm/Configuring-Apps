c95afc44c029a69b676831c49e3710b80485e187
vasilisdmr <vasilisdmr@gmail.com>
2019-07-27 15:02:05 +0300

    Replaced the CreateDefaultBuilder with individual statements that are called to create the default configuration.

--- Configuring-Apps/Program.cs
+++ Configuring-Apps/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace Configuring_Apps
 {
@@ -17,8 +18,41 @@ namespace Configuring_Apps
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) => {
+                    var env = hostingContext.HostingEnvironment;
+                    config.AddJsonFile("appsettings.json", optional: true,
+                            reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json",
+                            optional: true, reloadOnChange: true);
+                    if (env.IsDevelopment())
+                    {
+                        var appAssembly =
+                            Assembly.Load(new AssemblyName(env.ApplicationName));
+                        if (appAssembly != null)
+                        {
+                            config.AddUserSecrets(appAssembly, optional: true);
+                        }
+                    }
+                    config.AddEnvironmentVariables();
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                })
+                .ConfigureLogging((hostingContext, logging) => {
+                    logging.AddConfiguration(
+                        hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                }).UseIISIntegration()
+                  .UseDefaultServiceProvider((context, options) => {
+                        options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+        }).UseStartup<Startup>();
+        }                
     }
 }
