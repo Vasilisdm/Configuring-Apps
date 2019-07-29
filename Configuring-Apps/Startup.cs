322c386709e5b12832c423ca1a7f3a381c818f7a
vasilisdmr <vasilisdmr@gmail.com>
2019-07-29 23:05:35 +0300

    Modified the Configure method to use only the ContentMiddleware.

--- Configuring-Apps/Startup.cs
+++ Configuring-Apps/Startup.cs
@@ -23,7 +23,8 @@ namespace Configuring_Apps
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseMvcWithDefaultRoute();
+            app.UseMiddleware<ContentMiddleware>();
+            //app.UseMvcWithDefaultRoute();
         }
     }
 }
