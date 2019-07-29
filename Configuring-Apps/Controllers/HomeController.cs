ab80d71311aef18b387eebaa945bf94daeae4daa
vasilisdmr <vasilisdmr@gmail.com>
2019-07-29 22:17:59 +0300

    Added a dependency to UptimeService and passed the value of the timer to the index view.

--- Configuring-Apps/Controllers/HomeController.cs
+++ Configuring-Apps/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Configuring_Apps.Infrastructure;
 
 namespace Configuring_Apps.Controllers
 {
     public class HomeController : Controller
     {
+        private UptimeService _uptime;
+
+        public HomeController(UptimeService up)
+        {
+            _uptime = up;
+        }
+
         public ViewResult Index() => View(new Dictionary<string, string>
         {
-            ["Message"] = "This is the Index action method."
+            ["Message"] = "This is the Index action method.",
+            ["Uptime"] = $"{_uptime.Uptime} ms"
         });
     }
 }
