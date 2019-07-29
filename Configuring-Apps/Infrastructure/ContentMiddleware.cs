0803ab34ddf40b49393541eafe6baf12162d918d
vasilisdmr <vasilisdmr@gmail.com>
2019-07-29 23:05:16 +0300

    A middleware that simply produces a text if the "middleware" is found in the url.

---
+++ Configuring-Apps/Infrastructure/ContentMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Configuring_Apps.Infrastructure
+{
+    public class ContentMiddleware
+    {
+        private RequestDelegate _nextDelegate;
+
+        public ContentMiddleware(RequestDelegate next)
+        {
+            _nextDelegate = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Path.ToString() == "/middleware")
+            {
+                await httpContext.Response.WriteAsync("This is from the content middleware", Encoding.UTF8);
+            }
+            else
+            {
+                await _nextDelegate.Invoke(httpContext);
+            }
+        }
+    }
+}
