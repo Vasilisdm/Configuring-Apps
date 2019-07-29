c3138c8dfa378d77a36969724b23f21c5a8729fd
vasilisdmr <vasilisdmr@gmail.com>
2019-07-29 21:40:03 +0300

    Implemented a simple stopwatch, which will start counting the elapsed time as soon as an instance of the UptimeService is created.

--- Configuring-Apps/Infrastructure/UptimeService.cs
+++ Configuring-Apps/Infrastructure/UptimeService.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using System.Diagnostics;
+
 namespace Configuring_Apps.Infrastructure
 {
     public class UptimeService
     {
+        private Stopwatch _timer;
+
         public UptimeService()
         {
+            _timer = Stopwatch.StartNew();
         }
+
+        public long Uptime => _timer.ElapsedMilliseconds;
     }
 }
