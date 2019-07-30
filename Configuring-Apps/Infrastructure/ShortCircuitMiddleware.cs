0f722cbede35cec18e2090b5f92c98cf4d292157
vasilisdmr <vasilisdmr@gmail.com>
2019-07-30 22:45:30 +0300

    Created a simple ShortCircuitMiddleware, which returns a 403 status code if the user is using Microsoft Edge.

---
+++ Configuring-Apps/Infrastructure/ShortCircuitMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Configuring_Apps.Infrastructure
+{
+    public class ShortCircuitMiddleware
+    {
+        private RequestDelegate _nextDelegate;
+
+        public ShortCircuitMiddleware(RequestDelegate next)
+        {
+            _nextDelegate = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Headers["User-Agent"].Any(h => h.ToLower().Contains("edge")))
+            {
+                httpContext.Response.StatusCode = 403;
+            }
+            else
+            {
+                await _nextDelegate.Invoke(httpContext);
+            }
+        }
+    }
+}
