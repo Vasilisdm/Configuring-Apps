8eeffe4717daaa778810d87699c02e834715bd98
vasilisdmr <vasilisdmr@gmail.com>
2019-07-30 22:32:52 +0300

    Passing the UptimeService to the constructor of the ContentMiddleware.

--- Configuring-Apps/Infrastructure/ContentMiddleware.cs
+++ Configuring-Apps/Infrastructure/ContentMiddleware.cs
@@ -7,17 +7,20 @@ namespace Configuring_Apps.Infrastructure
     public class ContentMiddleware
     {
         private RequestDelegate _nextDelegate;
+        private UptimeService _uptime;
 
-        public ContentMiddleware(RequestDelegate next)
+        public ContentMiddleware(RequestDelegate next, UptimeService up)
         {
             _nextDelegate = next;
+            _uptime = up;
         }
 
         public async Task Invoke(HttpContext httpContext)
         {
             if (httpContext.Request.Path.ToString() == "/middleware")
             {
-                await httpContext.Response.WriteAsync("This is from the content middleware", Encoding.UTF8);
+                await httpContext.Response.WriteAsync("This is from the content middleware" +
+                                                      $"(uptime: {_uptime.Uptime} ms)", Encoding.UTF8);
             }
             else
             {
