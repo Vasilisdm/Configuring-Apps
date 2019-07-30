f4746b4e5e9fa6460d0abbc3404eb228f4a389c0
vasilisdmr <vasilisdmr@gmail.com>
2019-07-30 23:05:30 +0300

    This component inspects the header and stores the value within the Items dictionary at the key EdgeBrowser.

---
+++ Configuring-Apps/Infrastructure/BrowserTypeMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Configuring_Apps.Infrastructure
+{
+    public class BrowserTypeMiddleware
+    {
+        private RequestDelegate _nextDelegate;
+
+        public BrowserTypeMiddleware(RequestDelegate next)
+        {
+            _nextDelegate = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Items["EdgeBrowser"] = httpContext.Request.Headers["User-Agent"]
+                                               .Any(v => v.ToLower().Contains("edge"));
+
+            await _nextDelegate.Invoke(httpContext);
+        }
+    }
+}
