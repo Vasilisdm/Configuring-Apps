0f2bde6530a4d3bd81aa7e3ae715853a981382a4
vasilisdmr <vasilisdmr@gmail.com>
2019-07-31 23:16:56 +0300

    Added the ability to generate exceptions on demand within the index method.

--- Configuring-Apps/Controllers/HomeController.cs
+++ Configuring-Apps/Controllers/HomeController.cs
@@ -13,10 +13,20 @@ namespace Configuring_Apps.Controllers
             _uptime = up;
         }
 
-        public ViewResult Index() => View(new Dictionary<string, string>
+        public ViewResult Index(bool throwException = false)
         {
-            ["Message"] = "This is the Index action method.",
-            ["Uptime"] = $"{_uptime.Uptime} ms"
-        });
+            if (throwException)
+            {
+                throw new System.NullReferenceException();
+            }
+            else
+            {
+                return View(new Dictionary<string, string>
+                {
+                    ["Message"] = "This is the Index action method.",
+                    ["Uptime"] = $"{_uptime.Uptime} ms"
+                });
+            }
+        }
     }
 }
