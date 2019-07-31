0aa95719e183f97d16348e10727c04e7e51a55f9
vasilisdmr <vasilisdmr@gmail.com>
2019-07-31 23:56:54 +0300

    Added the ConfigureAppConfiguration in order to handle the configuration data.

--- Configuring-Apps/Program.cs
+++ Configuring-Apps/Program.cs
@@ -23,6 +23,14 @@ namespace Configuring_Apps
             return new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((hostingContext, config) => {
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                 })
                 .UseIISIntegration()
                 .UseStartup<Startup>();
         }
