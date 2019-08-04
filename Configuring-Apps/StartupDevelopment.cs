6805bedae117dbdfb468eecd95eb3fa8d402ea2d
vasilisdmr <vasilisdmr@gmail.com>
2019-08-04 21:14:11 +0300

    Created and implemented a startup class which will be used when the environment is development.

---
+++ Configuring-Apps/StartupDevelopment.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Configuring_Apps.Infrastructure;
+
+namespace Configuring_Apps
+{
+    public class StartupDevelopment
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<UptimeService>();
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
