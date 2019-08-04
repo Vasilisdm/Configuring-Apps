76e1c6dd825b276fec7691eaeba2c4cb60d6f690
vasilisdmr <vasilisdmr@gmail.com>
2019-08-04 19:42:02 +0300

    Added a custom logging message within the HomeController.

--- Configuring-Apps/Controllers/HomeController.cs
+++ Configuring-Apps/Controllers/HomeController.cs
@@ -1,20 +1,25 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Configuring_Apps.Infrastructure;
+using Microsoft.Extensions.Logging;
 
 namespace Configuring_Apps.Controllers
 {
     public class HomeController : Controller
     {
         private UptimeService _uptime;
+        private ILogger<HomeController> _logger;
 
-        public HomeController(UptimeService up)
+        public HomeController(UptimeService up, ILogger<HomeController> log)
         {
             _uptime = up;
+            _logger = log;
         }
 
         public ViewResult Index(bool throwException = false)
         {
+            _logger.LogDebug($"Handled {Request.Path} at uptime: {_uptime.Uptime}");
+
             if (throwException)
             {
                 throw new System.NullReferenceException();
